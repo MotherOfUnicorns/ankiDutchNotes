3b7361b3aad4c6dd5870cc3913554586fdcd3332
yun <elfie_9999@yahoo.com>
2020-04-06 13:26:40 +0200

    add exceptions and parse results from other sources

--- scraper_lxml.py
+++ scraper_lxml.py
@@ -19,6 +19,22 @@ def _get_html_table(dutch, english):
     return f"<table>{columns}</table>"
 
 
+class NoExplanationException(Exception):
+    pass
+
+
+class ExplanationsDoNotMatchException(Exception):
+    pass
+
+
+class NoExampleException(Exception):
+    pass
+
+
+class ExamplesDoNotMatchException(Exception):
+    pass
+
+
 def get_mwb_html(word):
     url = f"https://www.mijnwoordenboek.nl/vertaal/nl/en/{word}"
     html = requests.get(url)
@@ -33,6 +49,8 @@ class NoteDefault:
         html_content = html.content.decode("utf-8")
         start_idx = html_content.find("NL>EN")
         end_idx = html_content.find("Overige bronnen")
+
+        self.html_content = html_content
         self.doc = lxml.html.fromstring(html_content[start_idx:end_idx])
 
     def parse_dutch_word(self):
@@ -45,7 +63,7 @@ class NoteDefault:
         else:
             dutch = sections[0].text_content()
 
-        self.dutch = dutch
+        return dutch
 
     def parse_misc(self):
         part_of_speech = self.doc.xpath(f'//div/h2[@class="inline"]/parent::*/text()')
@@ -67,7 +85,7 @@ class NoteDefault:
         misc.sort()
         misc = _join_with_br([part_of_speech] + misc)
 
-        self.misc = misc
+        return misc
 
     def parse_explanations(self):
         explanations_english = [
@@ -85,12 +103,15 @@ class NoteDefault:
         ]
 
         if (not explanations_english) and (not explanations_dutch):
-            raise Exception
+            raise NoExplanationException("No default explanations found on mwb")
         if len(explanations_english) != len(explanations_dutch):
-            raise Exception
+            raise ExplanationsDoNotMatchException(
+                "Number of Dutch explanations and English explanations do not match each other"
+            )
+
         explanations = _get_html_table(explanations_dutch, explanations_english)
 
-        self.explanations = explanations
+        return explanations
 
     def parse_examples(self):
         examples_dutch = [
@@ -103,29 +124,89 @@ class NoteDefault:
         ]
 
         if (not examples_english) and (not examples_dutch):
-            raise Exception
+            # TODO add word to all exceptions
+            raise NoExampleException("No example sentences found")
         if len(examples_english) != len(examples_dutch):
-            raise Exception
+            raise ExamplesDoNotMatchException(
+                "Examples do not have matchin Dutch and Endlish translations"
+            )
         examples = _get_html_table(examples_dutch, examples_english)
 
-        self.examples = examples
+        return examples
 
     def generate_notes(self):
         if ERR_STRING in self.doc.text_content():
             return None
 
-        self.parse_dutch_word()
-        self.parse_misc()
-        self.parse_explanations()
-        self.parse_examples()
+        dutch = self.parse_dutch_word()
+        misc = self.parse_misc()
+
+        try:
+            explanations = self.parse_explanations()
+        except NoExplanationException:
+            explanations = self.parse_explanations_other_sources()
+
+        try:
+            examples = self.parse_examples()
+        except NoExampleException:
+            examples = self.parse_examples_other_sources()
 
         notefields = {
-            "Dutch": self.dutch,
-            "Misc": self.misc,
-            "Explanations": self.explanations,
-            "Examples": self.examples,
+            "Dutch": dutch,
+            "Misc": misc,
+            "Explanations": explanations,
+            "Examples": examples,
         }
         return notefields
+
+    def parse_explanations_other_sources(self):
+        start_idx = self.html_content.find("Overige bronnen")
+        other_sources = lxml.html.fromstring(self.html_content[start_idx:])
+
+        xpath_english = '//table[@border=0]/tr/td[@style="padding-left:20px"]'
+        explanations_english = [
+            x.text_content() for x in other_sources.xpath(xpath_english)
+        ]
+        explanations_dutch = [
+            x.text_content()
+            for x in other_sources.xpath(f"{xpath_english}/preceding-sibling::*")
+        ]
+
+        if (not explanations_english) and (not explanations_dutch):
+            raise NoExplanationException("No explanations found in other sources")
+        if len(explanations_english) != len(explanations_dutch):
+            raise ExplanationsDoNotMatchException(
+                "Number of Dutch explanations and English explanations in other sources do not match each other"
+            )
+        explanations = _get_html_table(explanations_dutch, explanations_english)
+        return explanations
+
+    def parse_examples_other_sources(self, num_examples=5):
+        start_idx = self.html_content.find("Voorbeeldzinnen met `")
+        other_examples = lxml.html.fromstring(self.html_content[start_idx:])
+
+        examples_url = [
+            x.text_content()
+            for x in other_examples.xpath("//script[contains(., load)]")
+            if "zinnendiv" in x.text_content()
+        ][0]
+        examples_url = examples_url.split('load("')[-1].split('");')[0]
+
+        html_content = requests.get(examples_url).content.decode("utf-8")
+        doc = lxml.html.fromstring(html_content)
+
+        examples = [x.text_content() for x in doc.xpath("//ol/li")]
+        examples = [e[5:].split(" EN: ") for e in examples][
+            : min(len(examples), num_examples)
+        ]
+        examples_dutch = [e[0] for e in examples]
+        examples_english = [e[1] for e in examples]
+
+        if (not examples_english) and (not examples_dutch):
+            # TODO add word to all exceptions
+            raise NoExampleException("No example sentences found in other sources")
+        examples = _get_html_table(examples_dutch, examples_english)
+        return examples
 
 
 def get_note_simple(word):

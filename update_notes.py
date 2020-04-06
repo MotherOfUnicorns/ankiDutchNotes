964c92ea053c0b843c46a7a60c4994b199d640d3
yun <elfie_9999@yahoo.com>
2020-04-06 16:33:43 +0200

    add command line arguments for deck name

--- update_notes.py
+++ update_notes.py
@@ -25,7 +25,18 @@ def update_existing_note_in_deck(note_id, deck_name):
 
 
 if __name__ == "__main__":
-    deck_name = "tttest"
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser(description="name of anki deck to update")
+    parser.add_argument("-d", "--deck", help="name of deck")
+    args = parser.parse_args()
+
+    if args.deck is None:
+        print("no deck name supplied!")
+        sys.exit()
+
+    deck_name = args.deck
     all_notes_id = find_all_notes_in_deck(deck_name)
     for note_id in all_notes_id:
         try:

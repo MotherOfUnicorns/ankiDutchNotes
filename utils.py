d2d0fba8c60ee160710acf8f7342380de20f53c0
yun <elfie_9999@yahoo.com>
2020-04-06 22:47:20 +0200

    better html table for explanations and example sentences

--- utils.py
+++ utils.py
@@ -10,13 +10,15 @@ def _get_table_row_bgcolor(ct):
 
 
 def get_html_table(list_of_columnes):
-    rows = [f'<td>{"</td> <td>".join(x)}</td>' for x in zip(*list_of_columnes)]
+    col_width = 100 // len(list_of_columnes)
+    rows = [f'</td> <td width="{col_width}%">'.join(x) for x in zip(*list_of_columnes)]
+    rows = [f'<td width="{col_width}%"> {r} </td>' for r in rows]
     columns = [
         f'<tr bgcolor="{_get_table_row_bgcolor(ct)}">{row}</tr>'
         for ct, row in enumerate(rows)
     ]
     columns = " ".join(columns)
-    return f"<table>{columns}</table>"
+    return f'<table align="left">{columns}</table>'
 
 
 def pad_list(input_list, target_length):
